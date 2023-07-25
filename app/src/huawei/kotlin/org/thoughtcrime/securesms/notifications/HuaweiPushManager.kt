01e9d15872481d446fc62c13fdb9c7ecb67f67d4
andrew <andrewgallasch@gmail.com>
2023-07-25 11:38:06 +0930

    Add Huawei flavor

---
+++ app/src/huawei/kotlin/org/thoughtcrime/securesms/notifications/HuaweiPushManager.kt
@@ -0,0 +1,9 @@
+package org.thoughtcrime.securesms.notifications
+
+import android.content.Context
+
+class HuaweiPushManager(val context: Context): PushManager {
+    override fun refresh(force: Boolean) {
+
+    }
+}
---
+++ app/src/huawei/kotlin/org/thoughtcrime/securesms/notifications/HuaweiPushModule.kt
@@ -0,0 +1,27 @@
+package org.thoughtcrime.securesms.notifications
+
+import android.content.Context
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HuaweiPushModule {
+    @Provides
+    @Singleton
+    fun provideFirebasePushManager(
+        @ApplicationContext context: Context,
+    ) = HuaweiPushManager(context)
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class FirebaseBindingModule {
+    @Binds
+    abstract fun bindPushManager(firebasePushManager: HuaweiPushManager): PushManager
+}
