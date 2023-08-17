ae9d3810e156ce20c1522212369e506cefc0f089
andrew <andrewgallasch@gmail.com>
2023-08-17 14:02:22 +0930

    Fix website flavor TokenFetcher

--- app/src/website/kotlin/org/thoughtcrime/securesms/notifications/NoOpTokenFetcher.kt
+++ app/src/website/kotlin/org/thoughtcrime/securesms/notifications/NoOpTokenFetcher.kt
@@ -1,12 +1,9 @@
 package org.thoughtcrime.securesms.notifications
 
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class NoOpTokenFetcher @Inject constructor() : TokenFetcher {
-    override fun fetch(): Job = MainScope().launch { }
+    override suspend fun fetch(): String? = null
 }
