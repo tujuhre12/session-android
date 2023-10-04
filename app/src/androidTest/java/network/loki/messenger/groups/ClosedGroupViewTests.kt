81111ea54a7a0ae3a336e7494537db016d74c657
0x330a <92654767+0x330a@users.noreply.github.com>
2023-10-04 12:50:49 +1100

    feat: add in dexmaker inline to mock final classes with android tests (experiment)

--- app/src/androidTest/java/network/loki/messenger/groups/ClosedGroupViewTests.kt
+++ app/src/androidTest/java/network/loki/messenger/groups/ClosedGroupViewTests.kt
@@ -1,12 +1,9 @@
-package org.thoughtcrime.securesms.groups
+package network.loki.messenger.groups
 
 import android.content.Context
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import junit.framework.TestCase.assertNotNull
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import network.loki.messenger.libsession_util.util.Sodium
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
@@ -19,12 +16,12 @@ import org.session.libsession.utilities.TextSecurePreferences
 import org.session.libsignal.utilities.Hex
 import org.session.libsignal.utilities.IdPrefix
 import org.session.libsignal.utilities.SessionId
-import org.thoughtcrime.securesms.MainCoroutineRule
 import org.thoughtcrime.securesms.database.ConfigDatabase
 import org.thoughtcrime.securesms.database.Storage
 import org.thoughtcrime.securesms.dependencies.ConfigFactory
+import org.thoughtcrime.securesms.groups.CreateGroupState
+import org.thoughtcrime.securesms.groups.CreateGroupViewModel
 
-@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(MockitoJUnitRunner::class)
 class ClosedGroupViewTests {
 
@@ -36,13 +33,6 @@ class ClosedGroupViewTests {
         Hex.fromStringCondensed("0123456789abcdef0123456789abcdef00000000000000000000000000000000")
     private val keyPair = Sodium.ed25519KeyPair(seed)
     private val userSessionId = SessionId(IdPrefix.STANDARD, Sodium.ed25519PkToCurve25519(keyPair.pubKey))
-
-    @ExperimentalCoroutinesApi
-    @get:Rule
-    var mainCoroutineRule = MainCoroutineRule()
-
-    @get:Rule
-    var taskRule = InstantTaskExecutorRule()
 
     @Mock lateinit var textSecurePreferences: TextSecurePreferences
     lateinit var storage: Storage
@@ -63,7 +53,7 @@ class ClosedGroupViewTests {
     }
 
     @Test
-    fun `Should error on empty name`() {
+    fun tryCreateGroup_shouldErrorOnEmptyName() {
         val viewModel = createViewModel()
         val state = CreateGroupState(
             groupName = "",
@@ -75,7 +65,7 @@ class ClosedGroupViewTests {
     }
 
     @Test
-    fun `Should error on empty members`() {
+    fun tryCreateGroup_shouldErrorOnEmptyMembers() {
         val viewModel = createViewModel()
         val state = CreateGroupState(
             groupName = "group",
@@ -87,7 +77,7 @@ class ClosedGroupViewTests {
     }
 
     @Test
-    fun `Should work with valid name and members`() {
+    fun tryCreateGroup_shouldSucceedWithCorrectParameters() {
         val viewModel = createViewModel()
         val state = CreateGroupState(
             groupName = "group",
