862a47e7e359992fe178211b8d8b8f8b80d9eb10
0x330a <92654767+0x330a@users.noreply.github.com>
2023-10-11 15:22:39 +1100

    feat: add drop attachment download migration in case there are a lot of pending failed attachment downloads

--- app/src/main/java/org/thoughtcrime/securesms/database/helpers/SQLCipherOpenHelper.java
+++ app/src/main/java/org/thoughtcrime/securesms/database/helpers/SQLCipherOpenHelper.java
@@ -90,8 +90,10 @@ public class SQLCipherOpenHelper extends SQLiteOpenHelper {
   private static final int lokiV42                          = 63;
   private static final int lokiV43                          = 64;
 
+  private static final int lokiV44                          = 65;
+
   // Loki - onUpgrade(...) must be updated to use Loki version numbers if Signal makes any database changes
-  private static final int    DATABASE_VERSION         = lokiV43;
+  private static final int    DATABASE_VERSION         = lokiV44;
   private static final int    MIN_DATABASE_VERSION     = lokiV7;
   private static final String CIPHER3_DATABASE_NAME    = "signal.db";
   public static final String  DATABASE_NAME            = "signal_v4.db";
@@ -602,6 +604,10 @@ public class SQLCipherOpenHelper extends SQLiteOpenHelper {
 
       if (oldVersion < lokiV43) {
         db.execSQL(RecipientDatabase.getAddBlocksCommunityMessageRequests());
+      }
+
+      if (oldVersion < lokiV44) {
+        db.execSQL(SessionJobDatabase.dropAttachmentDownloadJobs);
       }
 
       db.setTransactionSuccessful();
