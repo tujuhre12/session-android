94f3fc5a5f3d7eb6a242c038ea97bc549d89e320
andrew <andrewgallasch@gmail.com>
2023-10-19 22:02:37 +1030

    Rename and empty RecoveryPasswordActivity

---
+++ app/src/main/java/org/thoughtcrime/securesms/onboarding/RecoveryPasswordActivity.kt
@@ -0,0 +1,50 @@
+package org.thoughtcrime.securesms.onboarding
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import network.loki.messenger.R
+import org.session.libsession.utilities.TextSecurePreferences
+import org.session.libsignal.crypto.MnemonicCodec
+import org.session.libsignal.utilities.hexEncodedPrivateKey
+import org.thoughtcrime.securesms.BaseActionBarActivity
+import org.thoughtcrime.securesms.crypto.IdentityKeyUtil
+import org.thoughtcrime.securesms.crypto.MnemonicUtilities
+
+class RecoveryPasswordActivity : BaseActionBarActivity() {
+
+    private val seed by lazy {
+        var hexEncodedSeed = IdentityKeyUtil.retrieve(this, IdentityKeyUtil.LOKI_SEED)
+        if (hexEncodedSeed == null) {
+            hexEncodedSeed = IdentityKeyUtil.getIdentityKeyPair(this).hexEncodedPrivateKey // Legacy account
+        }
+        val loadFileContents: (String) -> String = { fileName ->
+            MnemonicUtilities.loadFileContents(this, fileName)
+        }
+        MnemonicCodec(loadFileContents).encode(hexEncodedSeed!!, MnemonicCodec.Language.Configuration.english)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar!!.title = resources.getString(R.string.activity_recovery_password)
+    }
+
+    private fun revealSeed() {
+        TextSecurePreferences.setHasViewedSeed(this, true)
+    }
+
+    private fun copySeed() {
+        revealSeed()
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("Seed", seed)
+        clipboard.setPrimaryClip(clip)
+        Toast.makeText(this, R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show()
+    }
+}
+
+fun Context.startSeedActivity() {
+    Intent(this, RecoveryPasswordActivity::class.java).also(::startActivity)
+}
