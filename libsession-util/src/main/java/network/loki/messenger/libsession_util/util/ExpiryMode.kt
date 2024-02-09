181dd150285caf3d1513616551a93f633283c403
Andrew <andrewgallasch@gmail.com>
2024-02-09 11:12:17 +1030

    Fix non-sms messages not deserialisable by kryo

--- libsession-util/src/main/java/network/loki/messenger/libsession_util/util/ExpiryMode.kt
+++ libsession-util/src/main/java/network/loki/messenger/libsession_util/util/ExpiryMode.kt
@@ -4,9 +4,9 @@ import kotlin.time.Duration.Companion.seconds
 
 sealed class ExpiryMode(val expirySeconds: Long) {
     object NONE: ExpiryMode(0)
-    data class Legacy(private val seconds: Long): ExpiryMode(seconds)
-    data class AfterSend(private val seconds: Long): ExpiryMode(seconds)
-    data class AfterRead(private val seconds: Long): ExpiryMode(seconds)
+    data class Legacy(private val seconds: Long = 0L): ExpiryMode(seconds)
+    data class AfterSend(private val seconds: Long = 0L): ExpiryMode(seconds)
+    data class AfterRead(private val seconds: Long = 0L): ExpiryMode(seconds)
 
     val duration get() = expirySeconds.seconds
 
