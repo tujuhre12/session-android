505d839924bf178e2698ef822aa0c06351077258
Andrew <andrewgallasch@gmail.com>
2024-03-01 14:24:48 +1030

    Hide keyboard on open qr tab

--- app/src/main/java/org/thoughtcrime/securesms/onboarding/LinkDeviceActivity.kt
+++ app/src/main/java/org/thoughtcrime/securesms/onboarding/LinkDeviceActivity.kt
@@ -45,6 +45,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -132,7 +133,10 @@ class LinkDeviceActivity : BaseActionBarActivity() {
 
                 runCatching {
                     when (title) {
-                        R.string.activity_link_device_scan_qr_code -> cameraProvider.get().bindToLifecycle(LocalLifecycleOwner.current, selector, preview)
+                        R.string.activity_link_device_scan_qr_code -> {
+                            cameraProvider.get().bindToLifecycle(LocalLifecycleOwner.current, selector, preview)
+                            LocalSoftwareKeyboardController.current?.hide()
+                        }
                         else -> cameraProvider.get().unbind(preview)
                     }
                 }
