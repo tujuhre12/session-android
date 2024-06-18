c69f1c880372f4ad9ffdb19a0f2fa021930af91e
Andrew <andrewgallasch@gmail.com>
2024-06-18 22:03:17 +0930

    Fix import in EmptyView

--- app/src/main/java/org/thoughtcrime/securesms/home/EmptyView.kt
+++ app/src/main/java/org/thoughtcrime/securesms/home/EmptyView.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.unit.dp
 import network.loki.messenger.R
 import org.thoughtcrime.securesms.ui.Divider
 import org.thoughtcrime.securesms.ui.LocalDimensions
