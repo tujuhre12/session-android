df532c944842ad56333ed31590c9918db86a7cd3
Andrew <andrewgallasch@gmail.com>
2024-06-18 22:05:24 +0930

    Fix import in LoadAccount

--- app/src/main/java/org/thoughtcrime/securesms/onboarding/loadaccount/LoadAccount.kt
+++ app/src/main/java/org/thoughtcrime/securesms/onboarding/loadaccount/LoadAccount.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -19,7 +18,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.Flow
 import network.loki.messenger.R
 import org.thoughtcrime.securesms.onboarding.ui.ContinueButton
