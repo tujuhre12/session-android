27efe99910852e088f6a858159821fac1ce4fc3d
Andrew <andrewgallasch@gmail.com>
2024-06-18 22:08:38 +0930

    Fix imports in PickDisplayName

--- app/src/main/java/org/thoughtcrime/securesms/onboarding/pickname/PickDisplayName.kt
+++ app/src/main/java/org/thoughtcrime/securesms/onboarding/pickname/PickDisplayName.kt
@@ -1,19 +1,16 @@
 package org.thoughtcrime.securesms.onboarding.pickname
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import network.loki.messenger.R
 import org.thoughtcrime.securesms.onboarding.ui.ContinueButton
 import org.thoughtcrime.securesms.ui.LocalDimensions
