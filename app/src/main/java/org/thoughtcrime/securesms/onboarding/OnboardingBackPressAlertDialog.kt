95ecbe664d70cdafc6831970e78ce05cf5a3aa46
bemusementpark <bemusementpark>
2024-07-11 11:26:41 +0930

    SES-2386 Account Creation Modal

--- app/src/main/java/org/thoughtcrime/securesms/onboarding/OnboardingBackPressAlertDialog.kt
+++ app/src/main/java/org/thoughtcrime/securesms/onboarding/OnboardingBackPressAlertDialog.kt
@@ -1,5 +1,6 @@
 package org.thoughtcrime.securesms.onboarding
 
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import network.loki.messenger.R
@@ -11,12 +12,13 @@ import org.thoughtcrime.securesms.ui.color.LocalColors
 @Composable
 fun OnboardingBackPressAlertDialog(
     dismissDialog: () -> Unit,
+    @StringRes textId: Int = R.string.you_cannot_go_back_further_in_order_to_stop_loading_your_account_session_needs_to_quit,
     quit: () -> Unit
 ) {
     AlertDialog(
         onDismissRequest = dismissDialog,
         title = stringResource(R.string.warning),
-        text = stringResource(R.string.you_cannot_go_back_further_in_order_to_stop_loading_your_account_session_needs_to_quit),
+        text = stringResource(textId),
         buttons = listOf(
             DialogButtonModel(
                 GetString(stringResource(R.string.quit)),
--- app/src/main/java/org/thoughtcrime/securesms/onboarding/messagenotifications/MessageNotifications.kt
+++ app/src/main/java/org/thoughtcrime/securesms/onboarding/messagenotifications/MessageNotifications.kt
@@ -58,7 +58,7 @@ internal fun MessageNotificationsScreen(
         return
     }
 
-    if (state.showDialog) OnboardingBackPressAlertDialog(dismissDialog, quit)
+    if (state.showDialog) OnboardingBackPressAlertDialog(dismissDialog, quit = quit)
 
     Column {
         Spacer(Modifier.weight(1f))
