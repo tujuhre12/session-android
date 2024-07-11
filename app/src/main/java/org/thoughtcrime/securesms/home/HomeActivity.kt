e3b4636b42f13cdc9d647ef1dd08705da2202983
bemusementpark <bemusementpark>
2024-07-11 23:52:29 +0930

    Reopen app to pcurrently open activity

--- app/src/main/java/org/thoughtcrime/securesms/home/HomeActivity.kt
+++ app/src/main/java/org/thoughtcrime/securesms/home/HomeActivity.kt
@@ -138,6 +138,9 @@ class HomeActivity : PassphraseRequiredActionBarActivity(),
     // region Lifecycle
     override fun onCreate(savedInstanceState: Bundle?, isReady: Boolean) {
         super.onCreate(savedInstanceState, isReady)
+
+        if (!isTaskRoot) { finish(); return }
+
         // Set content view
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
