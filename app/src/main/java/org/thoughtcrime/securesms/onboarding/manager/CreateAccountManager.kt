96dded096f68823b5ade5bc25c5f829ec421da70
ThomasSession <thomas.r@getsession.org>
2024-08-06 12:24:42 +1000

    Making sure the username is saved when creating an account

--- app/src/main/java/org/thoughtcrime/securesms/onboarding/manager/CreateAccountManager.kt
+++ app/src/main/java/org/thoughtcrime/securesms/onboarding/manager/CreateAccountManager.kt
@@ -8,6 +8,7 @@ import org.session.libsignal.utilities.KeyHelper
 import org.session.libsignal.utilities.hexEncodedPublicKey
 import org.thoughtcrime.securesms.crypto.KeyPairUtilities
 import org.thoughtcrime.securesms.dependencies.ConfigFactory
+import org.thoughtcrime.securesms.util.ConfigurationMessageUtilities
 import org.thoughtcrime.securesms.util.VersionDataFetcher
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -24,7 +25,6 @@ class CreateAccountManager @Inject constructor(
 
     fun createAccount(displayName: String) {
         prefs.setProfileName(displayName)
-        configFactory.user?.setName(displayName)
 
         // This is here to resolve a case where the app restarts before a user completes onboarding
         // which can result in an invalid database state
@@ -37,12 +37,15 @@ class CreateAccountManager @Inject constructor(
         val x25519KeyPair = keyPairGenerationResult.x25519KeyPair
 
         KeyPairUtilities.store(application, seed, ed25519KeyPair, x25519KeyPair)
-        configFactory.keyPairChanged()
         val userHexEncodedPublicKey = x25519KeyPair.hexEncodedPublicKey
         val registrationID = KeyHelper.generateRegistrationId(false)
         prefs.setLocalRegistrationId(registrationID)
         prefs.setLocalNumber(userHexEncodedPublicKey)
         prefs.setRestorationTime(0)
+
+        configFactory.keyPairChanged()
+        configFactory.user?.setName(displayName)
+        ConfigurationMessageUtilities.forceSyncConfigurationNowIfNeeded(application)
 
         versionDataFetcher.startTimedVersionCheck()
     }
