021f950d87554b1c557dfe3d91a8ee77f59d5661
ThomasSession <thomas.r@getsession.org>
2025-05-12 08:33:24 +1000

    Allowing clicking on members in group memebrs screen

--- app/src/main/java/org/thoughtcrime/securesms/groups/GroupMembersViewModel.kt
+++ app/src/main/java/org/thoughtcrime/securesms/groups/GroupMembersViewModel.kt
@@ -1,30 +1,58 @@
 package org.thoughtcrime.securesms.groups
 
 import android.content.Context
+import android.content.Intent
+import androidx.lifecycle.viewModelScope
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.session.libsession.database.StorageProtocol
+import org.session.libsession.utilities.Address
 import org.session.libsession.utilities.ConfigFactoryProtocol
 import org.session.libsession.utilities.UsernameUtils
 import org.session.libsignal.utilities.AccountId
+import org.thoughtcrime.securesms.conversation.v2.ConversationActivityV2
 import org.thoughtcrime.securesms.util.AvatarUtils
 
 
 @HiltViewModel(assistedFactory = GroupMembersViewModel.Factory::class)
 class GroupMembersViewModel @AssistedInject constructor(
     @Assisted private val groupId: AccountId,
-    @ApplicationContext context: Context,
-    storage: StorageProtocol,
+    @ApplicationContext private val context: Context,
+    private val storage: StorageProtocol,
     configFactory: ConfigFactoryProtocol,
     usernameUtils: UsernameUtils,
     avatarUtils: AvatarUtils
 ) : BaseGroupMembersViewModel(groupId, context, storage, usernameUtils, configFactory, avatarUtils) {
 
+    private val _navigationActions = Channel<Intent>()
+    val navigationActions = _navigationActions.receiveAsFlow()
+
     @AssistedFactory
     interface Factory {
         fun create(groupId: AccountId): GroupMembersViewModel
+    }
+
+    fun onMemberClicked(accountId: AccountId) {
+        viewModelScope.launch(Dispatchers.Default) {
+            val address = Address.fromSerialized(accountId.hexString)
+            val threadId = storage.getThreadId(address)
+
+            val intent = Intent(
+                context,
+                ConversationActivityV2::class.java
+            )
+            intent.putExtra(ConversationActivityV2.ADDRESS, address)
+            intent.putExtra(ConversationActivityV2.THREAD_ID, threadId)
+
+            _navigationActions.send(intent)
+        }
     }
 }
