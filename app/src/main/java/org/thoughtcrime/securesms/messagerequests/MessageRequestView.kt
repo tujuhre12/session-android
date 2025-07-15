c53ff420381922a5b74792dc8d8dd0564f5e8cbc
jbsession <jb@getsession.org>
2025-07-15 14:09:13 +0800

    Added unread styling for message requests

--- app/src/main/java/org/thoughtcrime/securesms/messagerequests/MessageRequestView.kt
+++ app/src/main/java/org/thoughtcrime/securesms/messagerequests/MessageRequestView.kt
@@ -2,9 +2,13 @@ package org.thoughtcrime.securesms.messagerequests
 
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.drawable.ColorDrawable
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import network.loki.messenger.R
 import network.loki.messenger.databinding.ViewMessageRequestBinding
@@ -13,6 +17,8 @@ import org.thoughtcrime.securesms.conversation.v2.utilities.MentionUtilities.hig
 import org.thoughtcrime.securesms.database.model.ThreadRecord
 import com.bumptech.glide.RequestManager
 import org.thoughtcrime.securesms.util.DateUtils
+import org.thoughtcrime.securesms.util.UnreadStylingHelper
+import org.thoughtcrime.securesms.util.getAccentColor
 import java.util.Locale
 import javax.inject.Inject
 
@@ -38,11 +44,30 @@ class MessageRequestView : LinearLayout {
 
         val senderDisplayName = getUserDisplayName(thread.recipient) ?: thread.recipient.address.toString()
         val unreadCount = thread.unreadCount
+        val isUnread = unreadCount > 0 && !thread.isRead
+
+        binding.root.background = UnreadStylingHelper.getUnreadBackground(context, isUnread)
+
+        //Add block logic for accent here if necessary
+        val accentColor = context.getAccentColor()
+        val background = ColorDrawable(accentColor)
+        binding.accentView.background = background
+        binding.accentView.visibility = if (unreadCount > 0 && !thread.isRead) View.VISIBLE else View.INVISIBLE
+
+        binding.unreadCountTextView.apply {
+            text = UnreadStylingHelper.formatUnreadCount(unreadCount)
+            setTextSize(
+                TypedValue.COMPLEX_UNIT_DIP,
+                UnreadStylingHelper.getUnreadTextSize(unreadCount)
+            )
+        }
+        binding.unreadCountIndicator.isVisible =  (unreadCount != 0 && !thread.isRead)
 
         binding.displayNameTextView.text = senderDisplayName
         binding.timestampTextView.text = dateUtils.getDisplayFormattedTimeSpanString(
             thread.date
         )
+
         val snippet = highlightMentions(
             text = thread.getDisplayBody(context),
             formatOnly = true, // no styling here, only text formatting
