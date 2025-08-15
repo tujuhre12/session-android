bdd100bfad816fa89a28d4a821f6aa21d4ab7f68
JBsesh <jb.mateo02@gmail.com>
2025-08-15 14:27:02 +0800

    Added min width to Hide button

--- app/src/main/java/org/thoughtcrime/securesms/ui/theme/Dimensions.kt
+++ app/src/main/java/org/thoughtcrime/securesms/ui/theme/Dimensions.kt
@@ -21,6 +21,7 @@ data class Dimensions(
     val minItemButtonHeight: Dp = 50.dp,
     val minLargeItemButtonHeight: Dp = 60.dp,
     val minButtonWidth: Dp = 160.dp,
+    val minSmallButtonWidth: Dp = 50.dp,
 
     val indicatorHeight: Dp = 4.dp,
 
