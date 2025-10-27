98af979fc944df48bec5096dfb698d6257370003
SessionHero01 <180888785+SessionHero01@users.noreply.github.com>
2025-10-28 09:49:32 +1100

    Bump version code to 248

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ configurations.configureEach {
     exclude(module = "commons-logging")
 }
 
-val canonicalVersionCode = 427
+val canonicalVersionCode = 428
 val canonicalVersionName = "1.28.2"
 
 val postFixSize = 10
